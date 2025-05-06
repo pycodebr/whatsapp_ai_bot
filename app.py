8ef75fc9c9ebfef7fdcaa8f56dc0fe1828c6ec14
pycodebr <pycodebr@gmail.com>
2025-05-06 20:10:08 -0300

    feat: add debounce messages

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI, Request
 
-from chains import get_conversational_rag_chain
-from evolution_api import send_whatsapp_message
+from message_buffer import buffer_message
 
 
 app = FastAPI()
-
-conversational_rag_chain = get_conversational_rag_chain()
 
 @app.post('/webhook')
 async def webhook(request: Request):
@@ -15,14 +12,9 @@ async def webhook(request: Request):
     message = data.get('data').get('message').get('conversation')
 
     if chat_id and message and not '@g.us' in chat_id:
-        ai_response = conversational_rag_chain.invoke(
-            input={'input': message},
-            config={'configurable': {'session_id': chat_id}},
-        )['answer']
-
-        send_whatsapp_message(
-            number=chat_id,
-            text=ai_response,
+        await buffer_message(
+            chat_id=chat_id,
+            message=message,
         )
 
     return {'status': 'ok'}
